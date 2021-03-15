754f176cc38654da26c8c001e4d24b05ec5d4554
Danilo Novaes <57879816+danilo-1@users.noreply.github.com>
2021-03-14 21:38:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# javaEE-juros_simples
+ atividade desenvolvida para o curso de Programação Orientada à objeto onde foi feito um programa que lê as 3 entradas fornecida pelo usuário e mostre o valor futuro da conta
---
+++ src/java/servlet/JurosSimplesServlet.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author usuário
+ */
+public class JurosSimplesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>java02- juros simples</title>");
+            out.println("<link rel='stylesheet' href='style.css'/>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3><a href='index.html'>Voltar</a></h3>");
+            out.println("<h1>Juros simples</h1>");
+            out.println("<div>");
+            try {
+                double vp = Double.parseDouble(request.getParameter("vp"));
+                out.println("<h3>Valor Presente = "+vp+"</h3>");
+                double juros = Double.parseDouble(request.getParameter("juros"));
+                out.println("<h3>Juros = "+juros+"</h3>");
+                double tempo = Double.parseDouble(request.getParameter("tempo"));
+                out.println("<h3>Tempo = "+tempo+"</h3>");
+                double vf = vp+(1+juros*tempo);
+                out.println("<h2>Valor Futuro será: " + vf + "</h2>");
+             }catch(Exception ex){
+                out.println("<h2>houve um erro: "+ex.getLocalizedMessage()+"</h1>");
+            }
+             out.println("</div>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
